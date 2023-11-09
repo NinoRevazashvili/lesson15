91f4e944e6a2f0d9ae8af88485a575bacac3d7a7
Nino <146452228+NinoRevazashvili@users.noreply.github.com>
2023-11-09 23:35:00 +0400

    script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+var Name=("Nino");
+console.log(Name);
+
+let age=22;
+console.log(age);
+
+let isMarried=false;
+console.log(isMarried);
+
+let person=null;
+person={
+    Name:"Nino",
+    age:22,
+    isMarried:false,
+
+};
+console.log("my Name is" + " " + person.Name);
+
+console.log(`My name is ${person.Name} and I am ${person.age} years old`);
+
+var x = 15;
+var y = 15;
+var z = 20;
+
+var sum = y + z + x;
+
+console.log(sum);
+
+var x = 15;
+var y = 15;
+var z = 20;
+
+var deduct = y -z - x;
+
+console.log(deduct);
+
+var x = 15;
+var y = 15;
+var z = 20;
+
+var multiply = y*z* x;
+
+console.log(multiply);
+
+var x = 15;
+var y = 15;
+var z = 20;
+
+var devide = y/z/x;
+
+console.log(devide);
+
+let length=7;
+let width=5;
+
+let area=7*5
+
+console.log(area)
